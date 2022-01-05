ececc6d6ae04859ff5c2629acfd298910b390ae0
rcktfalcon <diogo.lamounier@estudante.ufjf.br>
2022-01-05 00:09:42 -0300

    confirmar

---
+++ Coleções/script.js
@@ -0,0 +1,17 @@
+function getAdmins(map) {
+    let admins = []
+    for([key, value] of map) {
+        if(value === 'Admin') {
+            admins.push(key)
+        }
+    }
+    return admins;
+}
+
+const usuarios = new Map();
+
+usuarios.set('Luiz', 'Admin')
+usuarios.set('Roberta', 'Admin')
+usuarios.set('Jorge', 'User')
+
+console.log(getAdmins(usuarios));
